e16eae1cf1ff1204f879c7fe595a07e684636ab6
Eugene <sifmeop@gmail.com>
2024-01-19 21:35:41 +0200

    added seo for index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { Home } from '~/screens/home'
 import { PageTitle } from '~/ui/page-title'
+import { Meta } from '~/utils/Meta'
 
 export default function HomePage() {
   return (
     <>
+      <Meta title='ПРОФИЛЬ' description='Профиль пользователя' />
       <PageTitle>ПРОФИЛЬ</PageTitle>
       <Home />
     </>
